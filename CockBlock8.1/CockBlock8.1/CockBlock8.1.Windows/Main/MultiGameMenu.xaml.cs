1cb14e3685516ffebed0cdf6decff333fb688ab5
rangemaster <rangemaster_r@hotmail.com>
2014-12-12 11:36:01 +0100

    Start Layout + Instruction Menu
    With working buttons

---
+++ CockBlock8.1/CockBlock8.1/CockBlock8.1.Windows/Main/MultiGameMenu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using CockBlock8._1.Main.Instructions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CockBlock8._1.Main
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MultiGameMenu : Page
+    {
+        public MultiGameMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void New_bn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Search_bn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Instructions_bn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(InstructionsMultiGame), e);
+        }
+
+        private void Back_bn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), e);
+        }
+    }
+}
--- CockBlock8.1/CockBlock8.1/CockBlock8.1.Windows/Main/SingleGameMenu.xaml.cs
+++ CockBlock8.1/CockBlock8.1/CockBlock8.1.Windows/Main/SingleGameMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CockBlock8._1.Main.Instructions;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,16 +16,30 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace CockBlock8._1
+namespace CockBlock8._1.Main
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SingleGameMenu : Page
     {
-        public MainPage()
+        public SingleGameMenu()
         {
             this.InitializeComponent();
+        }
+
+        private void Start_bn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Instruction_bn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(InstructionsSingleGame), e);
+        }
+
+        private void Back_bn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), e);
         }
     }
 }
