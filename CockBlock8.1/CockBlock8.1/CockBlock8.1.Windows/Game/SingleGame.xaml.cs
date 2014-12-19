e52bd76282b25c4f33794361f546ac997fb667d7
rangemaster <rangemaster_r@hotmail.com>
2014-12-19 11:47:34 +0100

    Rotating Health status text
    Wel done :+1:

--- CockBlock8.1/CockBlock8.1/CockBlock8.1.Windows/Game/SingleGame.xaml.cs
+++ CockBlock8.1/CockBlock8.1/CockBlock8.1.Windows/Game/SingleGame.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CockBlock8._1.View;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,11 +21,31 @@ namespace CockBlock8._1.Game
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SingleGame : Page
+    public sealed partial class SingleGame : CB_Page
     {
         public SingleGame()
         {
             this.InitializeComponent();
+        }
+
+        internal void NextFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddShot(int shieldCannonIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SwitchGoingUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetEnergy(int p, int cannon, int energy)
+        {
+            throw new NotImplementedException();
         }
     }
 }
